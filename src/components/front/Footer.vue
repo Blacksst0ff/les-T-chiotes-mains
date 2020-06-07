b371c558f0298bd9ca2e2a1e5a1f4743e88ff2d5
Kévin <bernier.kevin59@gmail.com>
2020-06-07 19:09:39 +0200

    Opti Footer

--- src/components/front/Footer.vue
+++ src/components/front/Footer.vue
@@ -4,7 +4,7 @@
     color="#292929"
     height="60"
   >
-    <div class="title font-weight-light grey--text text--lighten-1 text-center">
+    <div class="subtitle-1 font-weight-light grey--text text--lighten-1 text-center">
       &copy; {{ (new Date()).getFullYear() }} — Les T'chiotes mains — Fait avec 💜 by Kévin Bernier
     </div>
   </v-footer>
