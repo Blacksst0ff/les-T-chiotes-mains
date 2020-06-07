17dc880347cbca5d3ca853b0da21be624da04ae6
Kévin <bernier.kevin59@gmail.com>
2020-06-07 18:26:19 +0200

    Improve header logo link

--- src/components/front/Header.vue
+++ src/components/front/Header.vue
@@ -5,15 +5,25 @@
     dense
   >
     <v-img
+      link
       contain
       max-height="70%"
       :src="require('@/assets/logo_t.png')"
+      style="cursor: pointer;"
+      @click="goHome()"
     />
   </v-app-bar>
 </template>
 
 <script>
 export default {
+  methods: {
+    goHome () {
+      if (this.$route.name != 'home') {
+        return this.$router.push({ name: 'home' });
+      }
+    }
+  }
 }
 </script>
 
